8b8c60d7c7fbcbdfeb9c458c675e1402f3db5666
Việt Nguyễn Thế <83101689+theviet17@users.noreply.github.com>
2024-11-13 02:03:59 +0700

    UPDATE

---
+++ Test/My project/Assets/Level15Manager.cs
@@ -0,0 +1,72 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Level15Manager : MonoBehaviour
+{
+    public List<SamePoint> points = new List<SamePoint>();
+    bool Win;
+    bool End;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckColider();
+        }
+    }
+
+    void CheckColider()
+    {
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Thực hiện raycast từ vị trí con trỏ chuột
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero, 10);
+
+        // Kiểm tra nếu raycast trúng một collider
+        if (hit.collider != null)
+        {
+            Debug.Log("Found Diem at objecxt: " + hit.collider.name);
+            SamePoint diemFound = points.FirstOrDefault(x => x.points.Contains(hit.collider.gameObject));// ==  || x.point2 == hit.collider.gameObject);
+            if (diemFound != null)
+            {
+                diemFound.GoGo();
+                points.Remove(diemFound);
+                if (points.Count <= 0)
+                {
+                    Win = true;
+                }
+                
+            }
+
+        }
+    }
+}
+
+[Serializable]
+public class SamePoint
+{
+    public List<GameObject> points;
+    public bool actice;
+
+    public SpriteRenderer cirle1;
+    public SpriteRenderer cirle2;
+
+    public void GoGo()
+    {
+        if (!actice)
+        {
+            actice = true;
+            cirle1.gameObject.SetActive(true);
+            cirle2.gameObject.SetActive(true);
+
+            UIAnimation.Fade(cirle1, 0.5f, true, 1);
+            UIAnimation.Fade(cirle2, 0.5f, true, 1);
+        }
+
+    }
+}
---
+++ Test/My project/Assets/Level16Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level16Manager : MonoBehaviour
+{
+    public RuoiController ruoiController1;
+    public RuoiController ruoiController2;
+
+    public List<GameObject> ruois;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckColider();
+        }
+    }
+
+    private void CheckColider()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Thực hiện raycast từ vị trí con trỏ chuột
+        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero, 10);
+
+        // Kiểm tra nếu raycast trúng một collider
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+            RuoiController ruoiController = hit.collider.GetComponent<RuoiController>();
+            if (ruoiController != null)
+            {
+                ruoiController.RuoiDeath();
+
+                var newRuoi = Instantiate(ruois[Random.Range(0, ruois.Count)]);
+                ruoiController.ruoi = newRuoi.transform;
+                ruoiController.StartFlying();
+            }
+
+            //if (hit.collider.gameObject.)
+            //Debug.Log("Found Diem at objecxt: " + hit.collider.name);
+            //SamePoint diemFound = points.FirstOrDefault(x => x.points.Contains(hit.collider.gameObject));// ==  || x.point2 == hit.collider.gameObject);
+            //if (diemFound != null)
+            //{
+            //    diemFound.GoGo();
+            //    points.Remove(diemFound);
+            //    if (points.Count <= 0)
+            //    {
+            //        Win = true;
+            //    }
+
+                //}
+
+        }
+    }
+
+}
